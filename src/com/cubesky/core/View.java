96d2aaaaf7074d4cc5d33671493978c97f856413
cubesky <max_301@live.com>
2017-01-02 17:29:58 +0800

    Support Windows

--- src/com/cubesky/core/View.java
+++ src/com/cubesky/core/View.java
@@ -9,37 +9,39 @@ import java.util.stream.Stream;
  */
 public class View {
     public static String getTagView(){
+        String linesplit=(System.getProperty("os.name").toLowerCase().contains("windows") ?"\r\n":"\n");
         StringBuilder builder=new StringBuilder();
         Supplier<Stream<String>> stringStream=() -> ((TreeSet<String>)Stor.stor.get("gfw")).stream();
-        builder.append("! Regex").append("\n");
+        builder.append("! Regex")
+                .append(linesplit);
         stringStream.get()
                 .filter(s -> (!s.startsWith("|")&&(!s.startsWith("@@"))))
-                .forEach(s -> builder.append(s).append("\n"));
-        builder.append("\n")
+                .forEach(s -> builder.append(s).append(linesplit));
+        builder.append(linesplit)
                 .append("! Subdomain")
-                .append("\n");
+                .append(linesplit);
         stringStream.get()
                 .filter(s -> s.startsWith("|")&&(!s.startsWith("||")))
-                .forEach(s -> builder.append(s).append("\n"));
+                .forEach(s -> builder.append(s).append(linesplit));
         for(int i=0;i<=9;i++){
-            builder.append("\n")
+            builder.append(linesplit)
                     .append("! ")
                     .append(String.valueOf(i))
-                    .append("\n");
+                    .append(linesplit);
             final int fi=i;
             stringStream.get()
                     .filter(s -> (s.startsWith("||"))&&Character.toString(s.charAt(2)).equalsIgnoreCase(String.valueOf(fi)))
-                    .forEach(s -> builder.append(s).append("\n"));
+                    .forEach(s -> builder.append(s).append(linesplit));
         }
         for(int i=(int)'A';i<(int)'A'+26;i++){
-            builder.append("\n")
+            builder.append(linesplit)
                     .append("! ")
                     .append(Character.toString((char)i))
-                    .append("\n");
+                    .append(linesplit);
             final int fi=i;
             stringStream.get()
                     .filter(s -> (s.startsWith("||"))&&(Character.toString(s.charAt(2)).equalsIgnoreCase(String.valueOf(Character.toString((char)fi)))))
-                    .forEach(s -> builder.append(s).append("\n"));
+                    .forEach(s -> builder.append(s).append(linesplit));
         }
         return builder.toString();
     }
