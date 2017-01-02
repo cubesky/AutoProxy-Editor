05044ae9e530327bc184d8a29f1885c29c81b2f6
cubesky <max_301@live.com>
2017-01-02 17:02:58 +0800

    Change Line Separator to UNIX style

--- src/com/cubesky/core/View.java
+++ src/com/cubesky/core/View.java
@@ -11,35 +11,35 @@ public class View {
     public static String getTagView(){
         StringBuilder builder=new StringBuilder();
         Supplier<Stream<String>> stringStream=() -> ((TreeSet<String>)Stor.stor.get("gfw")).stream();
-        builder.append("! Regex").append(System.getProperty("line.separator"));
+        builder.append("! Regex").append("\n");
         stringStream.get()
                 .filter(s -> (!s.startsWith("|")&&(!s.startsWith("@@"))))
-                .forEach(s -> builder.append(s).append(System.getProperty("line.separator")));
-        builder.append(System.getProperty("line.separator"))
+                .forEach(s -> builder.append(s).append("\n"));
+        builder.append("\n")
                 .append("! Subdomain")
-                .append(System.getProperty("line.separator"));
+                .append("\n");
         stringStream.get()
                 .filter(s -> s.startsWith("|")&&(!s.startsWith("||")))
-                .forEach(s -> builder.append(s).append(System.getProperty("line.separator")));
+                .forEach(s -> builder.append(s).append("\n"));
         for(int i=0;i<=9;i++){
-            builder.append(System.getProperty("line.separator"))
+            builder.append("\n")
                     .append("! ")
                     .append(String.valueOf(i))
-                    .append(System.getProperty("line.separator"));
+                    .append("\n");
             final int fi=i;
             stringStream.get()
                     .filter(s -> (s.startsWith("||"))&&Character.toString(s.charAt(2)).equalsIgnoreCase(String.valueOf(fi)))
-                    .forEach(s -> builder.append(s).append(System.getProperty("line.separator")));
+                    .forEach(s -> builder.append(s).append("\n"));
         }
         for(int i=(int)'A';i<(int)'A'+26;i++){
-            builder.append(System.getProperty("line.separator"))
+            builder.append("\n")
                     .append("! ")
                     .append(Character.toString((char)i))
-                    .append(System.getProperty("line.separator"));
+                    .append("\n");
             final int fi=i;
             stringStream.get()
                     .filter(s -> (s.startsWith("||"))&&(Character.toString(s.charAt(2)).equalsIgnoreCase(String.valueOf(Character.toString((char)fi)))))
-                    .forEach(s -> builder.append(s).append(System.getProperty("line.separator")));
+                    .forEach(s -> builder.append(s).append("\n"));
         }
         return builder.toString();
     }
